97366164943c377185535d428e63cac8113dd9a8
RmiTtro <tetreault.remi@gmail.com>
2024-03-17 22:04:16 -0400

    added Samba config to hosts

---
+++ hosts/common/samba/default.nix
@@ -0,0 +1,32 @@
+{
+  inputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  services.samba = {
+    enable = true;
+    securityType = "user";
+    openFirewall = true;
+    extraConfig = ''
+      workgroup = WORKGROUP
+      server string = ${config.hostname}
+      netbios name = ${config.hostname}
+      security = user 
+      #use sendfile = yes
+      #max protocol = smb2
+      # note: localhost is the ipv6 localhost ::1
+      hosts allow = 192.168.1. 127.0.0.1 localhost
+      hosts deny = 0.0.0.0/0
+      guest account = nobody
+      map to guest = bad user
+    '';
+  };
+  
+  # This is to avertise the share
+  services.samba-wsdd = {
+    enable = true;
+    openFirewall = true;
+  };
+}
--- hosts/common/users/rtetreault/default.nix
+++ hosts/common/users/rtetreault/default.nix
@@ -11,6 +11,7 @@ userPasswordSecretConfigPath = [ "sops" "secrets" "users_password/${username}" ]
 userPasswordSecretPathConfigPath = userPasswordSecretConfigPath ++ [ "path" ];
 isUserPasswordSecretPathExists = lib.attrsets.hasAttrByPath userPasswordSecretPathConfigPath config;
 getUserPasswordSecretPath = lib.attrsets.getAttrFromPath userPasswordSecretPathConfigPath config;
+userHome = config.users.users."${username}".home;
 in {
   config = lib.mkMerge [
     {
@@ -41,7 +42,7 @@ in {
         # Import your home-manager configuration
         "${username}" = import ../../../../home/${username}/${config.hostname}.nix {
           username = config.users.users."${username}".name;
-          home = config.users.users."${username}".home;
+          home = userHome;
         };
       };
     }
@@ -51,5 +52,41 @@ in {
         neededForUsers = true;
       })
     )
+    
+    (lib.mkIf (config.services.samba.enable) {
+      services.samba.shares."${username}Public" = {
+        path = "${userHome}/Public";
+        browseable = "yes";
+        "read only" = "no";
+        "guest ok" = "no";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+        "force user" = "${username}";
+        "force group" = "users";
+      };
+    })
+    
+    (lib.mkIf (config.services.samba.enable && config ? "sops") {
+      sops.secrets."samba_passwords/${username}" = {};
+      
+      system.activationScripts = {
+        "${username}SambaAccountCreation" = {
+          text = let
+            sambaPasswordPath = config.sops.secrets."samba_passwords/${username}".path;
+          in
+          ''
+            if [ -f ${sambaPasswordPath} ]; then
+              # Create/modify user samba account with a blank password
+              (${pkgs.coreutils-full}/bin/echo ""; ${pkgs.coreutils-full}/bin/echo "") | ${pkgs.samba}/bin/pdbedit -u ${username} -a -t 1> /dev/null
+              # Modify the user samba account to set the password using the nt-hash from the secret file
+              ${pkgs.samba}/bin/pdbedit -u ${username} -r --set-nt-hash=$(${pkgs.coreutils-full}/bin/cat ${sambaPasswordPath}) 1> /dev/null
+              # Delete the secret file, it is not needed anymore
+              rm ${sambaPasswordPath}
+            fi
+          '';
+          deps = [ "setupSecrets" ]; # Make it execute after the sops-nix activation script
+        };
+      };
+    })
   ];
 }
