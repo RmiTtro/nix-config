603c9712726087113a87bda6705358126025697d
RmiTtro <tetreault.remi@gmail.com>
2024-04-04 22:49:36 -0400

    handle cudatext plugins and themes as derivations

--- home/rtetreault/features/cudatext/default.nix
+++ home/rtetreault/features/cudatext/default.nix
@@ -6,19 +6,35 @@
   ...
 }: let
   cudaPluginsInputs = lib.attrsets.filterAttrs (name: value: lib.strings.hasPrefix "cuda_" name) inputs;
-  cudaPluginsConfigs = lib.attrsets.concatMapAttrs (name: value: {"cudatext/py/${name}".source = value;}) cudaPluginsInputs;
+  funcCudaPluginInputToDerivation = (name: value: (pkgs.runCommand name {src = value;} ''
+    mkdir -p $out
+    mkdir -p $out/${name}
+    cp -r $src/* $out/${name}
+  ''));
+  cudaPluginsDerivations = lib.attrsets.mapAttrsToList funcCudaPluginInputToDerivation cudaPluginsInputs;
+  cudaPluginsDirectory = pkgs.symlinkJoin { name = "cudatext_plugins"; paths = cudaPluginsDerivations; };
+  
   cudaThemesInputs = lib.attrsets.filterAttrs (name: value: lib.strings.hasPrefix "cudaTheme_" name) inputs;
-  cudaThemesFilesPath = builtins.concatMap (p: builtins.filter (lib.strings.hasInfix ".cuda-theme-") (lib.filesystem.listFilesRecursive p)) (builtins.attrValues cudaThemesInputs);
-  getLowerCaseBaseNameForKey = p: "${builtins.unsafeDiscardStringContext (lib.strings.toLower (builtins.baseNameOf p))}";
-  cudaThemesConfig = lib.attrsets.mergeAttrsList (builtins.map (p: {"cudatext/data/themes/${getLowerCaseBaseNameForKey p}".source = p;}) cudaThemesFilesPath);
+  funcCudaThemeInputToDerivation = (name: value: (pkgs.runCommand name {src = value;} ''
+    mkdir -p $out
+    cp -r $src/**/*.cuda-theme* $out
+  ''));
+  cudaThemesDerivations = lib.attrsets.mapAttrsToList funcCudaThemeInputToDerivation cudaThemesInputs;
+  cudaThemesDirectory = pkgs.symlinkJoin { name = "cudatext_themes"; paths = cudaThemesDerivations; };
 in 
 {
   home.packages = with pkgs; [ cudatext ];
 
-  xdg.configFile = cudaPluginsConfigs 
-                   // cudaThemesConfig 
-                   // { "cudatext/settings/user.json".source = ./user.json; };
-                   
+  xdg.configFile."cudatext/py" = {
+    source = cudaPluginsDirectory;
+    recursive = true;
+  };
+  xdg.configFile."cudatext/data/themes" = {
+    source = cudaThemesDirectory;
+    recursive = true;
+  };
+  xdg.configFile."cudatext/settings/user.json".source = ./user.json;
+
   programs.bash.sessionVariables = {
     EDITOR = "cudatext -n -nh -ns -nn";
   };
