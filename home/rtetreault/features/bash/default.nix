ffd9413d38fc424c92719fb1d4568393617da6a6
RmiTtro <tetreault.remi@gmail.com>
2024-11-11 14:20:23 -0500

    Added direnv to automatically enter nix develop environment in allowed directories

--- home/rtetreault/features/bash/default.nix
+++ home/rtetreault/features/bash/default.nix
@@ -5,5 +5,13 @@
   pkgs,
   ...
 }: {
+  # This is to allow "nix develop" to be run automatically for directories for which the following command has been run:
+  #   echo "use flake" >> .envrc && direnv allow
+  programs.direnv = {
+    enable = true;
+    enableBashIntegration = true;
+    nix-direnv.enable = true;
+  };
+
   programs.bash.enable = true;
 }
