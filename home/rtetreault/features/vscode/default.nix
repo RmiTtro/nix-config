ec70757a8f0eec042b1bff98922840b202783b1a
RmiTtro <tetreault.remi@gmail.com>
2025-08-26 11:29:44 -0400

    For VSCode, modified the user config to disable Github Copilot and made permanent the User storage

--- home/rtetreault/features/vscode/default.nix
+++ home/rtetreault/features/vscode/default.nix
@@ -18,9 +18,75 @@
     "workbench.colorTheme" = "Default Dark Modern";
     "telemetry.telemetryLevel" = "off";
     "files.hotExit" = "off";
+
+    # Everything below this comment is to disable Github Copilot integrations
+    "accessibility.verbosity.inlineChat" = false;
+    "accessibility.verbosity.panelChat" = false;
+    "ansible.lightspeed.suggestions.waitWindow" = 360000;
+    "chat.agent.enabled" = false;
+    "chat.agent.maxRequests" = 0;
+    "chat.commandCenter.enabled" = false;
+    "chat.detectParticipant.enabled" = false;
+    "chat.extensionTools.enabled" = false;
+    "chat.focusWindowOnConfirmation" = false;
+    "chat.implicitContext.enabled" = {
+        "panel" = "never";
+    };
+    "chat.instructionsFilesLocations" = {
+        ".github/instructions" = false;
+    };
+    "chat.mcp.discovery.enabled" = false;
+    "chat.mcp.enabled" = false;
+    "chat.modeFilesLocations" = {
+        ".github/chatmodes" = false;
+    };
+    "chat.promptFiles" = false;
+    "chat.promptFilesLocations" = {
+        ".github/prompts" = false;
+    };
+    "chat.sendElementsToChat.attachCSS" = false;
+    "chat.sendElementsToChat.attachImages" = false;
+    "chat.sendElementsToChat.enabled" = false;
+    "chat.setupFromDialog" = false;
+    "chat.useFileStorage" = false;
+    "dataWrangler.experiments.copilot.enabled" = false;
+    "github.copilot.editor.enableAutoCompletions" = false;
+    "github.copilot.editor.enableCodeActions" = false;
+    "github.copilot.enable" = false;
+    "github.copilot.nextEditSuggestions.enabled" = false;
+    "github.copilot.renameSuggestions.triggerAutomatically" = false;
+    "githubPullRequests.codingAgent.enabled" = false;
+    "githubPullRequests.experimental.chat" = false;
+    "gitlab.duoChat.enabled" = false;
+    "inlineChat.holdToSpeech" = false;
+    "inlineChat.lineNaturalLanguageHint" = false;
+    "inlineChat.accessibleDiffView" = "off";
+    "mcp" = {
+        "inputs" = [];
+        "servers" = {};
+    };
+    "notebook.experimental.generate" = false;
+    "python.analysis.aiCodeActions" = {
+        "convertFormatString" = false;
+        "convertLambdaToNamedFunction" = false;
+        "generateDocstring" = false;
+        "generateSymbol" = false;
+        "implementAbstractClasses" = false;
+    };
+    "python.experiments.enabled" = false;
+    "remote.SSH.experimental.chat" = false;
+    "telemetry.feedback.enabled" = false;
+    "terminal.integrated.initialHint" = false;
+    "workbench.editor.empty.hint" = "hidden";
   };
 
   programs.vscode.profiles.default.extensions = with inputs.nix-vscode-extensions.extensions."${pkgs.system}".vscode-marketplace; [
     jnoortheen.nix-ide
   ];
+
+  permanenceHomeWrap = {
+    directories = [
+      ".config/Code/User"
+    ];
+  };
 }
