c49f40de772523b3da1544fd6f33f5669f0b3231
RmiTtro <tetreault.remi@gmail.com>
2025-10-30 11:13:41 -0400

    Changed VPN provider to Mullvad

--- home/rtetreault/features/vopono/default.nix
+++ home/rtetreault/features/vopono/default.nix
@@ -6,15 +6,14 @@
   pkgs,
   ...
 }: {
-  home.packages = with pkgs; [ vopono voponojail openvpn ];
+  home.packages = with pkgs; [ vopono voponojail openvpn wireguard-tools ];
 
   # Done this way since vopono does not like its config file to be readonly
   addCopyOnChange.xdg.configFile."vopono/config.toml" = {
     text = ''
-      provider = "NordVPN"
+      provider = "Mullvad"
+      protocol = "Wireguard"
       server = "canada"
-      # NordVPN DNS url retrived on https://support.nordvpn.com/hc/en-us/articles/19587726859793-What-are-the-addresses-of-my-NordVPN-DNS-servers
-      dns = ["103.86.96.100", "103.86.96.100"]
     '';
     # Also have to place the init file elsewhere since vopono does not want any file to be readonly in its config directory
     initFileInSpecialDir = true;
